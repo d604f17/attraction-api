bbf9c32edc5557ee23f2c69e7f2a13d5a9be56ba
rasmus@rhnorskov.com <rasmus@rhnorskov.com>
2017-03-14 16:55:21 +0100

    v1

--- dist/main.js
+++ dist/main.js
@@ -8,16 +8,72 @@ var _lonelyplanetApi = require('lonelyplanet-api');
 
 var _lonelyplanetApi2 = _interopRequireDefault(_lonelyplanetApi);
 
+var _flickrApi = require('flickr-api');
+
+var _flickrApi2 = _interopRequireDefault(_flickrApi);
+
+var _geocodeApi = require('geocode-api');
+
+var _geocodeApi2 = _interopRequireDefault(_geocodeApi);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
+var flickr = new _flickrApi2.default('85f11febb88e3a4d49342a95b7bcf1e8', 'json');
+var geocode = new _geocodeApi2.default('AIzaSyDfZBp51fjQZwk4QogCZIUtRaz8z96G0Ks', 'json');
 var lp = new _lonelyplanetApi2.default();
+
+app.get('/', function (req, res) {
+    res.json({
+        name: 'attractions-api',
+        version: '0.0.1',
+        authors: ['Rasmus Nørskov (rhnorskov)', 'Mathias Wieland (mathias.wieland)', 'Andreas Sommerset (asomm)']
+    });
+});
 
 app.get('/:country*', function (req, res) {
     lp.city(req.params.country + req.params[0]).then(function (city) {
         return city.sights();
     }).then(function (sights) {
-        res.send(sights);
+        sights.splice(5);
+
+        var geocodes = sights.map(function (sight) {
+            return geocode.query(sight.city.country + ' ' + sight.city.city + ' ' + sight.name);
+        });
+
+        return new Promise(function (resolve, reject) {
+            Promise.all(geocodes).then(function (data) {
+                var locations = data.map(function (geocode) {
+                    return geocode.results[0].geometry.location;
+                });
+
+                resolve(sights.map(function (sight, index) {
+                    return Object.assign(sight, locations[index]);
+                }));
+            }).catch(reject);
+        });
+    }).then(function (sights) {
+        var photos = sights.map(function (sight) {
+            return flickr.query('photos.search', {
+                lat: sight.lat,
+                lon: sight.lng,
+                text: sight.name
+            });
+        });
+
+        return new Promise(function (resolve, reject) {
+            Promise.all(photos).then(function (data) {
+                var populaties = data.map(function (photos) {
+                    return photos.photos.total;
+                });
+
+                resolve(sights.map(function (sight, index) {
+                    return Object.assign(sight, { popularity: parseInt(populaties[index]) });
+                }));
+            }).catch(reject);
+        });
+    }).then(function (sights) {
+        res.json(sights);
     }).catch(console.error);
 });
 
--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 import express from 'express';
 import LonelyPlanet from 'lonelyplanet-api';
+import Flickr from 'flickr-api';
+import Geocode from 'geocode-api';
 
 const app = express();
+const flickr = new Flickr('85f11febb88e3a4d49342a95b7bcf1e8', 'json');
+const geocode = new Geocode('AIzaSyDfZBp51fjQZwk4QogCZIUtRaz8z96G0Ks', 'json');
 const lp = new LonelyPlanet();
+
+app.get('/', (req, res) => {
+    res.json({
+        name: 'attractions-api',
+        version: '0.0.1',
+        authors: [
+            'Rasmus Nørskov (rhnorskov)',
+            'Mathias Wieland (mathias.wieland)',
+            'Andreas Sommerset (asomm)'
+        ]
+    })
+});
 
 app.get('/:country*', (req, res) => {
     lp.city(req.params.country + req.params[0])
@@ -10,7 +26,51 @@ app.get('/:country*', (req, res) => {
             return city.sights();
         })
         .then(sights => {
-            res.send(sights);
+            sights.splice(5);
+
+            let geocodes = sights.map(sight => {
+                return geocode.query(`${sight.city.country} ${sight.city.city} ${sight.name}`);
+            });
+
+            return new Promise((resolve, reject) => {
+                Promise.all(geocodes)
+                    .then(data => {
+                        let locations = data.map(geocode => {
+                            return geocode.results[0].geometry.location;
+                        });
+
+                        resolve(sights.map((sight, index) => {
+                            return Object.assign(sight, locations[index]);
+                        }));
+                    })
+                    .catch(reject);
+            });
+        })
+        .then(sights => {
+            let photos = sights.map(sight => {
+                return flickr.query('photos.search', {
+                    lat: sight.lat,
+                    lon: sight.lng,
+                    text: sight.name
+                })
+            });
+
+            return new Promise((resolve, reject) => {
+                Promise.all(photos)
+                    .then(data => {
+                        let populaties = data.map(photos => {
+                            return photos.photos.total;
+                        });
+
+                        resolve(sights.map((sight, index) => {
+                            return Object.assign(sight, {popularity: parseInt(populaties[index])});
+                        }));
+                    })
+                    .catch(reject);
+            });
+        })
+        .then(sights => {
+            res.json(sights);
         })
         .catch(console.error)
 });
